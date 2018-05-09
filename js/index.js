f15cf2d604e1e26ad687704ae2d6e297dd93095a
Anantaya038 <kikz.anantaya@gmail.com>
2018-05-09 23:58:31 +0700

    edit getproduct

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ ons.platform.select('ios');
 $(function () {
   Service.getProducts().then(function (message, datas) {
     datas.forEach(data => {
-      if (data.id % 2) {
+      if (data.productID % 2) {
         var template = $('#productsTemplate').html();
         var rendered = Mustache.render(template, data);
         $('#resultsCol1').append(rendered);
